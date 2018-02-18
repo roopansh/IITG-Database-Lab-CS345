2df7729885adef2c91ec34cd0989cf025a30a363
roopansh <roopansh.bansal@gmail.com>
2018-02-19 03:14:40 +0530

    Design Department Database LEFT

--- 4/150101053.sql
+++ 4/150101053.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE IF NOT EXISTS 09feb2018;
+CREATE DATABASE 09feb2018;
 
 USE 09feb2018;
 
 CREATE TABLE Course(
                     course_id CHAR(10) NOT NULL,
                     division ENUM('I', 'II', 'III', 'IV', 'NA') DEFAULT 'NA',
-                    CONSTRAINT PK_COURSE PRIMARY KEY (course_id, division)
+                    CONSTRAINT PK_COURSE_COURSEIDDIVISION PRIMARY KEY (course_id, division)
 );
 
 CREATE TABLE Department(
                         department_id CHAR(10) NOT NULL,
                         name CHAR(50) NOT NULL,
-                        CONSTRAINT PK_DEPARTMENT PRIMARY KEY (department_id)
+                        CONSTRAINT PK_DEPARTMENT_DEPTARTMENTID PRIMARY KEY (department_id)
 );
 
 CREATE TABLE Slot(
@@ -19,24 +19,25 @@ CREATE TABLE Slot(
                   day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') NOT NULL,
                   start_time TIME NOT NULL,
                   end_time TIME NOT NULL,
-                  CONSTRAINT PK_SLOT PRIMARY KEY (letter, day)
+                  CONSTRAINT PK_SLOT_LETTERDAY PRIMARY KEY (letter, day),
+                  CONSTRAINT CHECK_SLOT_TIME CHECK (start_time < end_time)
 );
 
 CREATE TABLE Room(
-                  room_number CHAR(10) NOT NULL,
+                  room_number CHAR(20) NOT NULL,
                   location ENUM('Core-I', 'Core-II', 'Core-III', 'Core-IV', 'LH', 'Local') NOT NULL,
-                  CONSTRAINT PK_ROOM PRIMARY KEY (room_number)
+                  CONSTRAINT PK_ROOM_ROOMNUMBER PRIMARY KEY (room_number)
 );
 
 CREATE TABLE ScheduledIn(
-                         course_id CHAR(10),
-                         course_division ENUM('I', 'II', 'III', 'IV', 'NA'),
-                         department CHAR(10),
-                         slot_letter ENUM('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'A1', 'B1', 'C1', 'D1', 'E1'),
+                         course_id CHAR(10) NOT NULL,
+                         course_division ENUM('I', 'II', 'III', 'IV', 'NA') NOT NULL,
+                         department CHAR(10) NOT NULL,
+                         slot_letter ENUM('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'A1', 'B1', 'C1', 'D1', 'E1') NOT NULL,
                          slot_day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'),
-                         room CHAR(10),
-                         CONSTRAINT FK_COURSE FOREIGN KEY (course_id, course_division) REFERENCES Course(course_id, division) ON DELETE CASCADE,
-                         CONSTRAINT FK_DEPARTMENT FOREIGN KEY (department) REFERENCES Department(department_id) ON DELETE CASCADE,
-                         CONSTRAINT FK_SLOT FOREIGN KEY (slot_letter, slot_day) REFERENCES Slot(letter, day) ON DELETE CASCADE,
-                         CONSTRAINT FK_ROOM FOREIGN KEY (room) REFERENCES Room(room_number) ON DELETE CASCADE
+                         room CHAR(20) NOT NULL,
+                         CONSTRAINT FK_SCHEDULEDIN_COURSE FOREIGN KEY (course_id, course_division) REFERENCES Course(course_id, division) ON DELETE CASCADE,
+                         CONSTRAINT FK_SCHEDULEDIN_DEPARTMENT FOREIGN KEY (department) REFERENCES Department(department_id) ON DELETE CASCADE,
+                         CONSTRAINT FK_SCHEDULEDIN_SLOT FOREIGN KEY (slot_letter, slot_day) REFERENCES Slot(letter, day) ON DELETE CASCADE,
+                         CONSTRAINT FK_SCHEDULEDIN_ROOM FOREIGN KEY (room) REFERENCES Room(room_number) ON DELETE CASCADE
 );
---
+++ 4/sql/150101053_department.sql
@@ -0,0 +1,16 @@
+USE 09feb2018;
+
+INSERT INTO Department(department_id, name) VALUES('BSBE', 'Bio Science & Bio Engineering');
+INSERT INTO Department(department_id, name) VALUES('CE', 'Chemical Engineering');
+INSERT INTO Department(department_id, name) VALUES('CEER', 'Center for Energy');
+INSERT INTO Department(department_id, name) VALUES('CH', 'Chemistry');
+INSERT INTO Department(department_id, name) VALUES('CL', 'Civil Engineering');
+INSERT INTO Department(department_id, name) VALUES('CRT', 'Center for Rural Technology');
+INSERT INTO Department(department_id, name) VALUES('CSE', 'Computer Science & Engineering');
+INSERT INTO Department(department_id, name) VALUES('DD', 'Design Department');
+INSERT INTO Department(department_id, name) VALUES('EEE', 'Electronics & Electrical Engineering');
+INSERT INTO Department(department_id, name) VALUES('HSS', 'Humanities & Social Sciences');
+INSERT INTO Department(department_id, name) VALUES('MA', 'Mathematics');
+INSERT INTO Department(department_id, name) VALUES('ME', 'Mechanical Engineering');
+INSERT INTO Department(department_id, name) VALUES('PH', 'Physics');
+INSERT INTO Department(department_id, name) VALUES('SAB', 'Students Academic Board Language Course');
