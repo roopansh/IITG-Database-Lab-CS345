1624437245f293161228aefe2fadae9fc583ef7f
roopansh <roopansh.bansal@gmail.com>
2018-02-24 11:52:39 +0530

    Assignment 6

--- 4/sql/150101053_department.sql
+++ 4/sql/150101053_department.sql
@@ -1,17 +1,17 @@
 USE 09feb2018;
 
 INSERT INTO Department(department_id, name) VALUES
-	('BSBE', 'Bio Science & Bio Engineering,,BSBE'),
-	('CE', 'Chemical Engineering,,CE'),
-	('CEER', 'Center for Energy,,CH'),
-	('CH', 'Chemistry,,CL'),
-	('CL', 'Civil Engineering,,CRT'),
-	('CRT', 'Center for Rural Technology,,CSE'),
-	('CSE', 'Computer Science & Engineering,,DD'),
-	('DD', 'Design Department,,EEE'),
-	('EEE', 'Electronics & Electrical Engineering,,EN'),
-	('HSS', 'Humanities & Social Sciences,,HSS'),
-	('MA', 'Mathematics,,MA'),
-	('ME', 'Mechanical Engineering,,ME'),
-	('PH', 'Physics,,PH'),
-	('SAB', 'Students Academic Board Language Course,,SAB');
+	('BSBE', 'Bio Science & Bio Engineering'),
+	('CE', 'Chemical Engineering'),
+	('CEER', 'Center for Energy'),
+	('CH', 'Chemistry'),
+	('CL', 'Civil Engineering'),
+	('CRT', 'Center for Rural Technology'),
+	('CSE', 'Computer Science & Engineering'),
+	('DD', 'Design Department'),
+	('EEE', 'Electronics & Electrical Engineering'),
+	('HSS', 'Humanities & Social Sciences'),
+	('MA', 'Mathematics'),
+	('ME', 'Mechanical Engineering'),
+	('PH', 'Physics'),
+	('SAB', 'Students Academic Board Language Course');
